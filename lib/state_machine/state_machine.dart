f022fcd9ea83a890009ab446021cec1262ca755d
Wafer Li <omyshokami@gmail.com>
2025-06-24 09:46:41 +0800

    feat: add state machine builder

--- lib/state_machine/state_machine.dart
+++ lib/state_machine/state_machine.dart
@@ -1,6 +1,43 @@
 import 'package:collection/collection.dart';
 
+class StateMachineBuilder<S, T, E, Side> {
+  S? _initialState;
+  List<TransitionDefinition<S, E, Side>> _transitionDefinitions = [];
+  List<TransitionListener<S, E, Side>> _transitionListeners = [];
+
+  void initialState(S state) {
+    this._initialState = state;
+  }
+
+  void state(Type state) {
+    // TODO: implement state transition builder
+  }
+
+  StateMachine<S, T, E, Side> build() {
+    final initialStateVal = _initialState;
+
+    if (_initialState == null) {
+      throw Exception('No initial state specified');
+    }
+
+    return StateMachine<S, T, E, Side>(
+      initialState: initialStateVal as S,
+      transitionDefinitions: _transitionDefinitions,
+      transitionListeners: _transitionListeners,
+    );
+  }
+}
+
 class StateMachine<S, T, E, Side> {
+
+  factory StateMachine.create(
+    void Function(StateMachineBuilder<S, T, E, Side>) builderBlock,
+  ) {
+    final builder = StateMachineBuilder<S, T, E, Side>();
+    builderBlock(builder);
+    return builder.build();
+  }
+
   StateMachine({
     required S initialState,
     required List<TransitionDefinition<S, E, Side>> transitionDefinitions,
@@ -35,11 +72,11 @@ class StateMachine<S, T, E, Side> {
     final transitionValid = validTransitionDefinition.to.call(event);
     final toState = transitionValid.to;
     currentState = toState;
-    
+
     for (final listener in _transitionListeners) {
       listener.onTransition(transitionValid);
     }
-    
+
     return toState;
   }
 }
