31c07e76f5a06a0f0f9c34c5dcab1294ab7085ed
Wafer Li <omyshokami@gmail.com>
2025-06-24 09:10:36 +0800

    feat: add state machine transition

--- lib/state_machine/state_machine.dart
+++ lib/state_machine/state_machine.dart
@@ -1,17 +1,19 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:collection/collection.dart';
 
 class StateMachine<S, T, E, Side> {
   StateMachine({
     required S initialState,
     required List<TransitionDefinition<S, E, Side>> transitionDefinitions,
+    required List<TransitionListener<S, E, Side>> transitionListeners,
   }) : currentState = initialState,
-       _transitionsDefinitions = transitionDefinitions;
+       _transitionsDefinitions = transitionDefinitions,
+       _transitionListeners = transitionListeners;
 
   S currentState;
 
   final List<TransitionDefinition<S, E, Side>> _transitionsDefinitions;
+
+  final List<TransitionListener<S, E, Side>> _transitionListeners;
 
   S? transition(E event) {
     final currentStateVal = currentState;
@@ -23,12 +25,21 @@ class StateMachine<S, T, E, Side> {
     );
 
     if (validTransitionDefinition == null) {
+      final transition = Transition(event, currentStateVal);
+      for (final listener in _transitionListeners) {
+        listener.onTransition(transition);
+      }
       return null;
     }
 
     final transitionValid = validTransitionDefinition.to.call(event);
     final toState = transitionValid.to;
     currentState = toState;
+    
+    for (final listener in _transitionListeners) {
+      listener.onTransition(transitionValid);
+    }
+    
     return toState;
   }
 }
@@ -50,4 +61,8 @@ class TransitionDefinition<S, E, Side> {
   final Type event;
   final Type from;
   final TransitionValid<S, E, Side> Function(E event) to;
+}
+
+abstract interface class TransitionListener<S, E, Side> {
+  void onTransition(Transition<S, E> transition);
 }
