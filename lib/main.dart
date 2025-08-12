39582756fc05372fe9d2fc390f90fc2fe9bde436
Wafer Li <omyshokami@gmail.com>
2025-08-12 08:57:52 +0800

    refactor: remove old crash logger

--- lib/main.dart
+++ lib/main.dart
@@ -4,11 +4,9 @@ import 'package:catcher_2/catcher_2.dart';
 import 'package:flutter/material.dart';
 import 'package:logger/web.dart';
 import 'package:ssb_runner/common/dirs.dart';
-import 'package:ssb_runner/crash_repoter.dart';
 import 'package:ssb_runner/ui/main_app/main_app.dart';
 
 final logger = Logger(printer: PrettyPrinter(methodCount: 5));
-final crashLogger = CrashLogger();
 const seedColor = Color(0xFF0059BA);
 
 void main() async {
