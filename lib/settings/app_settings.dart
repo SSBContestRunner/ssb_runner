f2009fe30d1a3f310afc8faf8838a5248dddf8bc
Wafer Li <omyshokami@gmail.com>
2025-10-21 23:49:20 +0800

    feat: add phonic type config

--- lib/settings/app_settings.dart
+++ lib/settings/app_settings.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:ssb_runner/audio/audio_loader.dart';
 import 'package:ssb_runner/common/constants.dart';
 import 'package:ssb_runner/contest_run/contests.dart';
 
@@ -11,14 +12,17 @@ class AppSettings {
 
   String get contestId =>
       _prefs.getString(_settingContestId) ?? supportedContests.first.id;
+
   set contestId(String value) => _prefs.setString(_settingContestId, value);
 
   String get contestModeId =>
       _prefs.getString(_settingContestMode) ?? supportedContestModes.first.id;
+
   set contestModeId(String value) =>
       _prefs.setString(_settingContestMode, value);
 
   String get stationCallsign => _prefs.getString(_settingStationCallsign) ?? '';
+
   set stationCallsign(String value) =>
       _prefs.setString(_settingStationCallsign, value);
 
@@ -30,8 +34,27 @@ class AppSettings {
   set contestDuration(int value) =>
       _prefs.setInt(_settingContestDuration, _limitContestDuration(value));
 
+  PhonicType get phonicType =>
+      _parsePhonicType(_prefs.getInt(_settingPhonicType));
+
+  set phonicType(PhonicType value) =>
+      _prefs.setInt(_settingPhonicType, value.index);
+
   int _limitContestDuration(int durationInMinutes) {
     return min(durationInMinutes, maxDurationInMinutesPerRun);
+  }
+
+  PhonicType _parsePhonicType(int? value) {
+    switch (value) {
+      case 0:
+        return PhonicType.standard;
+      case 1:
+        return PhonicType.location;
+      case 2:
+        return PhonicType.mixed;
+      default:
+        return PhonicType.standard;
+    }
   }
 }
 
@@ -39,3 +62,4 @@ const _settingContestId = 'setting_contest_id';
 const _settingContestMode = 'setting_contest_mode';
 const _settingStationCallsign = 'setting_station_callsign';
 const _settingContestDuration = 'setting_contest_duration';
+const _settingPhonicType = 'setting_phonic_type';
